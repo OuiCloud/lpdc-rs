d0d695e4dfed62b3110676936d51ec791ecdeffe
Philippe Menuet Tang <philippe@ouicloud.software>
2024-03-04 23:13:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<!-- cargo-sync-readme start -->
+
+# LDPC-RS 
+
+Pure Rust implemention of a selection of LDPC error correcting codes.
+
+It has no dependency neither `std`.
+
+<!-- cargo-sync-readme end -->
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
+
+//! # LDPC-RS 
+//! 
+//! Pure Rust implemention of a selection of LDPC error correcting codes.
+//!
+//! It has no dependency neither `std`.
